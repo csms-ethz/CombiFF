b786b8f1ea26d87540ab02e1587fc32c018ca450
SalomeRonja <salome.rieder@solnet.ch>
2021-10-29 17:55:49 +0200

    intialize documentation

---
+++ scr/compile_documentation.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import filecmp
+import shutil
+import glob
+import subprocess
+
+script_dir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(script_dir + "/../use")
+
+from global_settings import doc_dir
+
+def recompile(cff_man, cff_man_bu):
+  print("recompile")
+  os.system('./jcompile')
+  shutil.copyfile(cff_man, cff_man_bu)
+
+os.chdir(doc_dir + "/tex")
+
+for tex_file in glob.glob(doc_dir + "/tex/*.tex"):
+  doc_file = os.path.basename(tex_file)
+  doc_file_compiled = "." + doc_file
+  
+  pdf_file = glob.glob(doc_dir + "/" + "".join(doc_file.split('.')[:-1]) + ".pdf")
+  print(pdf_file)
+  print(doc_dir + "/" + "".join(doc_file.split('.')[:-1]) + ".pdf")
+
+  if not len(pdf_file) or not filecmp.cmp(doc_file, doc_file_compiled):
+    subprocess.run(["pdflatex", doc_file])
+    pdf_file = glob.glob(doc_dir + "/tex/*.pdf")[0]
+    shutil.copyfile(doc_file, doc_file_compiled)
+    shutil.move(pdf_file, doc_dir)
+
+  else:
+    print(doc_file + " is up to date")
