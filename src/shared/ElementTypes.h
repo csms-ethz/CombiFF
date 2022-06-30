eee9dac5167e172aeef84b205a7783d61922417f
Salomé Rieder <salome.rieder@solnet.ch>
2022-06-30 20:12:10 +0200

    generic priority ordering in molecular formulas

--- src/shared/ElementTypes.h
+++ src/shared/ElementTypes.h
@@ -30,35 +30,40 @@ struct ElementProperties {
 
   ElementProperties() = default;
   ElementProperties(ElementSymbol symbol, Connectivity degree,
-                    ElementNumber element_nr, ElementPriority priority,
-                    double mass, bool hydrogen_in_smiles)
+                    ElementNumber element_nr, double mass,
+                    bool hydrogen_in_smiles)
       : symbol(symbol),
         degree(degree),
         element_nr(element_nr),
-        priority(priority),
+        priority((ElementPriority)symbol[0]),
         mass(mass),
-        hydrogen_in_smiles(hydrogen_in_smiles) {}
+        hydrogen_in_smiles(hydrogen_in_smiles) {
+    if (symbol == "C")
+      priority = 0;
+    else if (symbol == "H")
+      priority = 1;
+  }
 };
 
 // IMPORTANT: element priority has to be unique for every element type!!
 static const std::unordered_map<ElementIdentifier, ElementProperties>
     element_property_map{
-        {"C", ElementProperties({"C", 4, 6, 0, 12.011, false})},
-        {"N", ElementProperties({"N", 3, 7, 3, 14.007, false})},
-        {"P", ElementProperties({"P", 3, 15, 5, 30.974, false})},
-        {"Pf", ElementProperties({"P", 5, 15, 5, 30.974, true})},
-        {"O", ElementProperties({"O", 2, 8, 2, 15.999, false})},
-        {"S", ElementProperties({"S", 2, 16, 4, 32.060, false})},
-        {"Sf", ElementProperties({"S", 4, 16, 4, 32.060, true})},
-        {"Ss", ElementProperties({"S", 6, 16, 4, 32.060, true})},
-        {"H", ElementProperties({"H", 1, 1, 1, hydrogen_mass, false})},
-        {"F", ElementProperties({"F", 1, 9, 6, 18.998, false})},
-        {"Cl", ElementProperties({"Cl", 1, 17, 7, 35.453, false})},
-        {"Br", ElementProperties({"Br", 1, 35, 8, 79.904, false})},
-        {"I", ElementProperties({"I", 1, 53, 9, 126.900, false})},
-        {"N+", ElementProperties({"N", 4, 7, 20, 14.007, true})},
-        {"B", ElementProperties({"B", 3, 5, 21, 10.81, false})},
-        {"Se", ElementProperties({"Se", 4, 34, 22, 78.96, true})},
+        {"C", ElementProperties({"C", 4, 6, 12.011, false})},
+        {"N", ElementProperties({"N", 3, 7, 14.007, false})},
+        {"P", ElementProperties({"P", 3, 15, 30.974, false})},
+        {"Pf", ElementProperties({"P", 5, 15, 30.974, true})},
+        {"O", ElementProperties({"O", 2, 8, 15.999, false})},
+        {"S", ElementProperties({"S", 2, 16, 32.060, false})},
+        {"Sf", ElementProperties({"S", 4, 16, 32.060, true})},
+        {"Ss", ElementProperties({"S", 6, 16, 32.060, true})},
+        {"H", ElementProperties({"H", 1, 1, hydrogen_mass, false})},
+        {"F", ElementProperties({"F", 1, 9, 18.998, false})},
+        {"Cl", ElementProperties({"Cl", 1, 17, 35.453, false})},
+        {"Br", ElementProperties({"Br", 1, 35, 79.904, false})},
+        {"I", ElementProperties({"I", 1, 53, 126.900, false})},
+        {"N+", ElementProperties({"N", 4, 7, 14.007, true})},
+        {"B", ElementProperties({"B", 3, 5, 10.81, false})},
+        {"Se", ElementProperties({"Se", 4, 34, 78.96, true})},
         // atom types for sadra's dihedrals
         /*
         {"Me", elementProperties({1, 100, 100, 0})},
@@ -69,7 +74,7 @@ static const std::unordered_map<ElementIdentifier, ElementProperties>
         {"HX", elementProperties({1, 105, 105, 0})},
         */
         // end of sadra's atom types
-        {"*", ElementProperties({"*", 0, 0, 100, 0, false})}};
+        {"*", ElementProperties({"*", 0, 0, 0, false})}};
 }  // namespace element_types
 
 }  // namespace combi_ff
